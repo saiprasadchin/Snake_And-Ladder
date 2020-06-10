98b10709be360124fc0d1b673795aac707b147f3
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-09 20:55:25 -0700

    SnakeLadderUC3

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,8 +2,28 @@ echo "Welcome to Snake and Ladder game"
 position=0
 echo "starting position is : $position"
 function dice(){
-rollDie=$((RANDOM % 6 + 1))
-echo "Dice : $rollDie "
+	rollDice=$((RANDOM % 6 + 1))
+
 }
-dice
+
+ladder=2
+snake=3
+noPlay=1
+
+check=$((RANDOM % 3 + 1))
+echo "check : $check"
+if [ $check -eq $noPlay ]
+then
+        echo "No play"
+elif [ $check -eq $snake ]
+then
+        dice
+        position=$(( $position - $rollDice ))
+        echo "Snack = player at position : $position"
+elif [ $check -eq $ladder ]
+then
+        dice
+        position=$(( $position + $rollDice ))
+        echo "Ladder = player at position : $position"
+fi
 
